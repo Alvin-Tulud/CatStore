5fd416db0411e5ea79692fb16b7591f01390f602
Alvin-Tulud <alvintulud@gmail.com>
2024-08-19 17:29:29 -0700

    start track stock

---
+++ CatStore/Assets/Scripts/StoreStatManager/Track_Stock.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TrackStock : MonoBehaviour
+{
+    List<Image> stock_items;
+    List<int> stock_count;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
